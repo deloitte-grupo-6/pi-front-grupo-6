9a476099d20b51a0cfb1c3885dc6cbbcf7efb6bb
Gabriela Vespoli <gabiat3@hotmail.com>
2021-10-24 14:02:37 -0300

    feat: customizing team component

--- src/app/views/home/components/team/team.component.ts
+++ src/app/views/home/components/team/team.component.ts
@@ -3,13 +3,38 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
-  styleUrls: ['./team.component.css']
+  styleUrls: ['./team.component.css'],
 })
 export class TeamComponent implements OnInit {
+  members = [
+    {
+      name: 'Gabriela',
+      lastname: 'Vespoli',
+      img: '../../../../../assets/images/woman.png',
+    },
+    {
+      name: 'Hugo',
+      lastname: 'Quadros',
+      img: '../../../../../assets/images/man.png',
+    },
+    {
+      name: 'Leandro',
+      lastname: 'M. C.',
+      img: '../../../../../assets/images/man.png',
+    },
+    {
+      name: 'Patricia',
+      lastname: 'Miyazaki',
+      img: '../../../../../assets/images/woman.png',
+    },
+    {
+      name: 'Rafael',
+      lastname: 'Gonzaga',
+      img: '../../../../../assets/images/man.png',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
