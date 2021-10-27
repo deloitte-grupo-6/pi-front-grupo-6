74b943f092ec5e99d47926f39606b6154f7d3227
Hugo Quadros <83436359+HugoQuadros@users.noreply.github.com>
2021-10-27 12:06:52 -0300

    botão cadastrar chamando modal, início do about

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  booleanCadastrar: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  modalCadastrar(){
+    this.booleanCadastrar = true;
   }
 
 }
