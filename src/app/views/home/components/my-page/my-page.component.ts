2e541dabe70302c3f63ea7b24d6a216a88e37bf6
Patricia <pmiyazakii@gmail.com>
2021-11-05 09:56:16 -0300

    Atualizado my-page

--- src/app/views/home/components/my-page/my-page.component.ts
+++ src/app/views/home/components/my-page/my-page.component.ts
@@ -8,7 +8,23 @@ import { UserService } from '../../services/user.service';
   styleUrls: ['./my-page.component.css'],
 })
 export class MyPageComponent implements OnInit {
+  public show: boolean = false;
+  public show2: boolean = false;
+  public show3: boolean = false;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  toggle() {
+    this.show = !this.show;
+  }
+
+  toggle2() {
+    this.show2 = !this.show2;
+  }
+
+  toggle3() {
+    this.show3 = !this.show3;
+  }
 }
